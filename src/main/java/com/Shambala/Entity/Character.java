8983f461c06f633a9830fa16ac92572ac3c3b97e
Corneloup Théo <cornelouptheo.pro@gmail.com>
2024-10-06 19:22:57 +0200

    Shambala RPG Project

---
+++ src/main/java/com/Shambala/Entity/Character.java
@@ -0,0 +1,4 @@
+package com.Shambala.Entity;
+
+public class Character {
+}
---
+++ src/main/java/com/Shambala/ShambalaApplication.java
@@ -0,0 +1,13 @@
+package com.Shambala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShambalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShambalaApplication.class, args);
+	}
+
+}

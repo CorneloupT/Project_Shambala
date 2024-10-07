47e2b646bda4095d7ddc7ef260bf27af33321ae6
CorneloupT <cornelouptheo.pro@gmail.com>
2024-10-07 13:54:15 +0200

    pom modification and first class Character

--- src/main/java/com/Shambala/Entity/Character.java
+++ src/main/java/com/Shambala/Entity/Character.java
@@ -1,4 +1,36 @@
 package com.Shambala.Entity;
 
-public class Character {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Character implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    @NotEmpty(message = "Ainsi donc tu n'as pas de nom ?")
+    private String name;
+
+    private String playerClass;
+    private String background;
+    private String race;
+    private String classExperience;
+    private String globalExperience;
+
+
 }

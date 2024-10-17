f59cabd98c99406fc976d9b6b1d6ecf93a587cf1
CorneloupT <cornelouptheo.pro@gmail.com>
2024-10-17 13:49:03 +0200

    remove unused import

--- src/test/java/com/Shambala/ServiceStatsTests/CreateCharacterStatsTest.java
+++ src/test/java/com/Shambala/ServiceStatsTests/CreateCharacterStatsTest.java
@@ -5,12 +5,6 @@ import com.Shambala.Service.ServiceImpl.CharacterStatServiceImpl;
 import com.Shambala.models.CharacterStats;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 

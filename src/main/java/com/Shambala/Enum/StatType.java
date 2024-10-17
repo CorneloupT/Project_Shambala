b343fa009db443d6be6487ab5d1c5d74eb8578a7
CorneloupT <cornelouptheo.pro@gmail.com>
2024-10-17 23:02:53 +0200

    new Enum statType

---
+++ src/main/java/com/Shambala/Enum/StatType.java
@@ -0,0 +1,19 @@
+package com.Shambala.Enum;
+
+public enum StatType {
+    PHY("Physical"),
+    DEX("Dexterity"),
+    PSY("Psychic"),
+    WIL("WillPower"),
+    FGT("Fight");
+
+    private final String statType;
+
+    StatType(String statType) {
+        this.statType = statType;
+    }
+
+    public String getRaceName() {
+        return statType;
+    }
+}

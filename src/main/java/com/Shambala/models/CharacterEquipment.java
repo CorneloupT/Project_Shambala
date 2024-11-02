5d74c4a7080e0e9ec416d9f30053abfa21c762af
CorneloupT <cornelouptheo.pro@gmail.com>
2024-11-02 14:37:41 +0100

    Refactor method in CharacterEquipment

--- src/main/java/com/Shambala/models/CharacterEquipment.java
+++ src/main/java/com/Shambala/models/CharacterEquipment.java
@@ -24,76 +24,32 @@ public class CharacterEquipment {
         characterEquipment.quality = equipmentBuilder.getQuality();
         characterEquipment.breakPoint = equipmentBuilder.getBreakPoint();
         characterEquipment.equipmentType = equipmentBuilder.getEquipmentType();
-        characterEquipment.verifyQualityNoviceHasBreakPoint60WithTypeWeapon();
-        characterEquipment.verifyQualityApprenticeHasBreakPoint40WithTypeWeapon();
-        characterEquipment.verifyQualityConfirmedHasBreakPoint25WithTypeWeapon();
-        characterEquipment.verifyQualityMasterHasBreakPoint10WithTypeWeapon();
-        characterEquipment.verifyQualityGrandMasterHasBreakPoint2WithTypeWeapon();
-        characterEquipment.verifyQualityNoviceHasBreakPoint75WithTypeArmor();
-        characterEquipment.verifyQualityApprenticeHasBreakPoint50WithTypeArmor();
-        characterEquipment.verifyQualityConfirmedHasBreakPoint35WithTypeArmor();
-        characterEquipment.verifyQualityMasterHasBreakPoint15WithTypeArmor();
-        characterEquipment.verifyQualityGrandMasterHasBreakPoint5WithTypeArmor();
+        characterEquipment.verifyWeaponPercentageBreakPointWhenQualityIsProvided();
+        characterEquipment.verifyArmorPercentageBreakPointWhenQualityIsProvided();
         return characterEquipment;
     }
 
-    private void verifyQualityNoviceHasBreakPoint60WithTypeWeapon() {
-        if (quality.equals(Quality.NOVICE) && equipmentType.equals(EquipmentType.WEAPON)) {
-            breakPoint = 60;
+    private void verifyWeaponPercentageBreakPointWhenQualityIsProvided() {
+        if (equipmentType.equals(EquipmentType.WEAPON)) {
+            breakPoint = switch (quality) {
+                case NOVICE -> 60;
+                case APPRENTICE -> 40;
+                case CONFIRMED -> 25;
+                case MASTER -> 10;
+                case GRAND_MASTER -> 2;
+            };
         }
     }
 
-    private void verifyQualityApprenticeHasBreakPoint40WithTypeWeapon() {
-        if (quality.equals(Quality.APPRENTICE) && equipmentType.equals(EquipmentType.WEAPON)) {
-            breakPoint = 40;
-        }
-    }
-
-    private void verifyQualityConfirmedHasBreakPoint25WithTypeWeapon() {
-        if (quality.equals(Quality.CONFIRMED) && equipmentType.equals(EquipmentType.WEAPON)) {
-            breakPoint = 25;
-        }
-    }
-
-    private void verifyQualityMasterHasBreakPoint10WithTypeWeapon() {
-        if (quality.equals(Quality.MASTER) && equipmentType.equals(EquipmentType.WEAPON)) {
-            breakPoint = 10;
-        }
-    }
-
-    private void verifyQualityGrandMasterHasBreakPoint2WithTypeWeapon() {
-        if (quality.equals(Quality.GRAND_MASTER) && equipmentType.equals(EquipmentType.WEAPON)) {
-            breakPoint = 2;
-        }
-    }
-
-    private void verifyQualityNoviceHasBreakPoint75WithTypeArmor() {
-        if (quality.equals(Quality.NOVICE) && equipmentType.equals(EquipmentType.ARMOR)) {
-            breakPoint = 75;
-        }
-    }
-
-    private void verifyQualityApprenticeHasBreakPoint50WithTypeArmor() {
-        if (quality.equals(Quality.APPRENTICE) && equipmentType.equals(EquipmentType.ARMOR)) {
-            breakPoint = 50;
-        }
-    }
-
-    private void verifyQualityConfirmedHasBreakPoint35WithTypeArmor() {
-        if (quality.equals(Quality.CONFIRMED) && equipmentType.equals(EquipmentType.ARMOR)) {
-            breakPoint = 35;
-        }
-    }
-
-    private void verifyQualityMasterHasBreakPoint15WithTypeArmor() {
-        if (quality.equals(Quality.MASTER) && equipmentType.equals(EquipmentType.ARMOR)) {
-            breakPoint = 15;
-        }
-    }
-
-    private void verifyQualityGrandMasterHasBreakPoint5WithTypeArmor() {
-        if (quality.equals(Quality.GRAND_MASTER) && equipmentType.equals(EquipmentType.ARMOR)) {
-            breakPoint = 5;
+    private void verifyArmorPercentageBreakPointWhenQualityIsProvided() {
+        if (equipmentType.equals(EquipmentType.ARMOR)) {
+            breakPoint = switch (quality) {
+                case NOVICE -> 75;
+                case APPRENTICE -> 50;
+                case CONFIRMED -> 35;
+                case MASTER -> 15;
+                case GRAND_MASTER -> 5;
+            };
         }
     }
 

d2d25475dbf4b551af9c2bdd3a2998f7cffc95e6
CorneloupT <cornelouptheo.pro@gmail.com>
2024-11-02 14:42:52 +0100

    refactor Enum EquipmentType and create class test for Damage

--- src/main/java/com/Shambala/Enum/EquipmentType.java
+++ src/main/java/com/Shambala/Enum/EquipmentType.java
@@ -4,9 +4,9 @@ public enum EquipmentType {
     ARMOR("ARMOR"),
     WEAPON("WEAPON");
 
-    private final String equipement;
+    private final String equipment;
 
-    EquipmentType(String equipementType) {
-        this.equipement = equipementType;
+    EquipmentType(String equipmentType) {
+        this.equipment = equipmentType;
     }
 }
---
+++ src/test/java/com/Shambala/Facade/DamageCalculatorFacadeTest.java
@@ -0,0 +1,4 @@
+package com.Shambala.Facade;
+
+public class DamageCalculatorFacadeTest {
+}
